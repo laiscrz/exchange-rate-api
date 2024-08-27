240ae42f9c4080937e2eaa9707f5bb6c4a6d091a
laiscrz <lais.albino2003@gmail.com>
2024-08-27 15:59:32 -0300

    feat: create Models

---
+++ Models/ConversionRate.cs
@@ -0,0 +1,7 @@
+namespace exchange_rate_api.Models
+{
+    public class ConversionRate : IConversionRate
+    {
+        public double BRL { get; set; }
+    }
+}
---
+++ Models/IConversionRate.cs
@@ -0,0 +1,7 @@
+namespace exchange_rate_api.Models
+{
+    public interface IConversionRate
+    {
+        double BRL { get; set; }
+    }
+}
